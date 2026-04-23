c8ca34ad380a5221e182747369b8bbb64954c8a2
unknown <appeimisic@gmail.com>
2026-04-23 12:49:51 +0100

    backend.sageconsolidated

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -38,7 +38,7 @@ import (
 // @contact.url    http://www.swagger.io/support
 // @contact.email  support@swagger.io
 
-// @host      16.171.140.125
+// @host      backend.sageconsolidated.com
 // @BasePath  /api/v1
 func main() {
 	cfg := config.SetupAPI()
