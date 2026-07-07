6f5d2a0920ace5166f770be754ae3108df8af1c1
unknown <appeimisic@gmail.com>
2026-07-07 21:43:43 +0100

    merged

--- internal/shield/middlewares/redis_limiter.go
+++ internal/shield/middlewares/redis_limiter.go
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package middlewares
 
 import (
@@ -48,55 +48,4 @@ func (r *RedisLimiterStore) Increment(
 	reset := windowStart.Add(window)
 
 	return int(countCmd.Val()), reset, nil
-=======
-package middlewares
-
-import (
-	"context"
-	"fmt"
-	"time"
-
-	"github.com/redis/go-redis/v9"
-)
-
-type RedisLimiterStore struct {
-	client *redis.Client
-}
-
-func NewRedisLimiterStore(client *redis.Client) *RedisLimiterStore {
-	return &RedisLimiterStore{
-		client: client,
-	}
-}
-
-func (r *RedisLimiterStore) Increment(
-	ctx context.Context,
-	key string,
-	limit int,
-	window time.Duration,
-) (int, time.Time, error) {
-
-	now := time.Now().UTC()
-	windowStart := now.Truncate(window)
-
-	redisKey := fmt.Sprintf(
-		"rate_limit:%s:%d",
-		key,
-		windowStart.Unix(),
-	)
-
-	pipe := r.client.TxPipeline()
-
-	countCmd := pipe.Incr(ctx, redisKey)
-	pipe.Expire(ctx, redisKey, window)
-
-	_, err := pipe.Exec(ctx)
-	if err != nil {
-		return 0, time.Time{}, err
-	}
-
-	reset := windowStart.Add(window)
-
-	return int(countCmd.Val()), reset, nil
->>>>>>> 3cbfcfda147bdc68807f3505d9e623e7af4a13bc
 }
