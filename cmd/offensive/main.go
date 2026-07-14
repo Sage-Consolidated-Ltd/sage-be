599a325ea7b24bba5f6e2b0eb2da50a8ed4be408
unknown <appeimisic@gmail.com>
2026-07-14 23:58:41 +0100

    docker deploy

--- cmd/offensive/main.go
+++ cmd/offensive/main.go
@@ -1,1 +1,9 @@
 package offensive
+
+
+import "log"
+
+func main() {
+	log.Println("Offensive service placeholder")
+	select {} // Keep the process alive
+}
--- cmd/vision/main.go
+++ cmd/vision/main.go
@@ -1,1 +1,9 @@
 package vision
+
+
+import "log"
+
+func main() {
+	log.Println("vision service placeholder")
+	select {} // Keep the process alive
+}
