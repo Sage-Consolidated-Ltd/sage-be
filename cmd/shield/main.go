e021da93a1f1481fcdd397f68700cc0271bbb0fa
unknown <appeimisic@gmail.com>
2026-07-14 22:20:07 +0100

    search on logs

--- cmd/shield/main.go
+++ cmd/shield/main.go
@@ -49,7 +49,7 @@ import (
 // @in cookie
 // @name session_id
 
-// @host      localhost:3335
+// @host      backend.sageconsolidated.com
 // @BasePath  /api/v1
 func main() {
 	cfg := config.SetupShield()
